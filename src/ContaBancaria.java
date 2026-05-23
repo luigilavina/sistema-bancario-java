27374a79956c0344f5586b36bb1828003981f712
Luigi Lavina <lavinaluigi@gmail.com>
2026-05-23 10:35:45 -0300

    implementando encapsulamento no sistema bancario

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,20 +1,32 @@
 public class ContaBancaria {
 
-    String titular;
-    int numeroConta;
-    float saldoConta;
+    private String titular;
+    private int numeroConta;
+    private double saldoConta;
 
-    ContaBancaria(String titular, int numeroConta, float saldoConta) {
+    public ContaBancaria(String titular, int numeroConta, double saldoConta) {
         this.titular = titular;
         this.numeroConta = numeroConta;
         this.saldoConta = saldoConta;
     }
 
-    public float depositar(float valorDepositado){
-        return saldoConta+= valorDepositado;
+    public String getTitular(){
+        return titular;
     }
 
-    public float sacar(float valorSaque){
-        return saldoConta-= valorSaque;
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+
+    public void depositar(double valorDepositado){
+        saldoConta+= valorDepositado;
+    }
+
+    public void sacar(double valorSaque){
+        saldoConta-= valorSaque;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,47 +10,53 @@ public class Main {
     System.out.println("Digite o número da conta");
     int numeroConta = scanner.nextInt();
 
-    float saldoConta = 500;
+    double saldoConta = 500;
 
     ContaBancaria contaBancaria = new ContaBancaria(titular, numeroConta, saldoConta);
 
     int escolha = 0;
-    while (escolha != 4){
-      System.out.println("Escolha uma das opções: 1-Depositar | 2-Sacar | 3-Mostrar saldo | 4-Sair : ");
+    while (escolha != 5){
+      System.out.println("Escolha uma das opções: 1-Depositar | 2-Sacar | 3-Mostrar saldo | 4-Mostrar Dados | 5-Sair : ");
       escolha = scanner.nextInt();
 
       switch (escolha){
         case 1: {
-          System.out.println("Seu saldo atual é de R$"+contaBancaria.saldoConta);
+          System.out.println("Seu saldo atual é de R$"+ contaBancaria.getSaldoConta());
           System.out.println("Qual o valor que voce quer depositar: ");
-          float valorDeposito = scanner.nextFloat();
+          double valorDeposito = scanner.nextDouble();
           if (valorDeposito > 0){
             contaBancaria.depositar(valorDeposito);
 
-            System.out.println("Agora o Saldo de sua conta é de R$"+contaBancaria.saldoConta);
+            System.out.println("Agora o Saldo de sua conta é de R$"+contaBancaria.getSaldoConta());
           }else {
             System.out.println("Digite um valor válido.");
           }
           break;
         }
         case 2: {
-          System.out.println("Seu saldo atual é de R$"+contaBancaria.saldoConta);
+          System.out.println("Seu saldo atual é de R$"+contaBancaria.getSaldoConta());
           System.out.println("Qual o valor que voce quer sacar: ");
-          float valorSaque = scanner.nextFloat();
+          double valorSaque = scanner.nextDouble();
 
-          if (valorSaque > 0 && valorSaque <= contaBancaria.saldoConta){
+          if (valorSaque > 0 && valorSaque <= contaBancaria.getSaldoConta()){
             contaBancaria.sacar(valorSaque);
-            System.out.println("Agora o Saldo de sua conta é de R$"+contaBancaria.saldoConta);
+            System.out.println("Agora o Saldo de sua conta é de R$"+contaBancaria.getSaldoConta());
           }else{
             System.out.println("Valor indisponivel para saque");
           }
           break;
         }
         case 3:{
-          System.out.println("Seu saldo atual é de R$"+contaBancaria.saldoConta);
+          System.out.println("Seu saldo atual é de R$"+ contaBancaria.getSaldoConta());
           break;
         }
         case 4:{
+          System.out.println("Nome: "+contaBancaria.getTitular());
+          System.out.println("Número da Conta: "+contaBancaria.getNumeroConta());
+          System.out.println(contaBancaria.getSaldoConta());
+          break;
+        }
+        case 5:{
           System.out.println("Saindo do sistema...");
           break;
         }
